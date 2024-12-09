20a94c35d51b6426a0476993b697e81f7dc4a44a
atul624900 <atulkumar624900@gmail.com>
2024-12-09 11:20:38 +0530

    assign value of a=7

---
+++ src/main/java/comPages/PracticeDemo.java
@@ -0,0 +1,39 @@
+package comPages;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class PracticeDemo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\atulk\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.icicibank.com/");
+		List<WebElement> link = driver.findElements(By.tagName("link"));
+		for (WebElement ele : link) {
+			String str = ele.getAttribute("href");
+			driver.get(str);
+		}
+		Set<String> linkId = driver.getWindowHandles();
+		Iterator<String> itr = linkId.iterator();
+		while (itr.hasNext()) {
+			int a = 7;
+
+			System.out.println(driver.getTitle());
+		}
+	}
+
+}
